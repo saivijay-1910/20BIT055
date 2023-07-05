d7ec621b7d47e845f626133f3df1f06722f709db
SAIVIJAY S <112149191+saivijay-1910@users.noreply.github.com>
2023-07-05 13:26:30 +0530

    Update server.js

--- Question1/server.js
+++ Question1/server.js
@@ -7,25 +7,30 @@ const server = http.createServer(function (req, res) {
   if (req.url == '/number') {
     res.write('{number :[1,2,5,8,13]}');
     res.end();
-  } else if (req.url == '/number/prime') {
+  }
+  else if (req.url == '/number/prime') {
     res.write('{number :[2,3,5,7,11,13,17,19,23,29,31,37,39,41,43,47]}');
     res.end();
-  } else if (req.url == '/number/fibo') {
+  }
+  else if (req.url == '/number/fibo') {
     res.write('{number :[0,1,1,2,3,5,8,13,21,34,55,89,144]}');
     res.end();
-  } else if (req.url == '/number/odd') {
+  } 
+  else if (req.url == '/number/odd') {
     res.write('{number :[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31]}');
     res.end();
-  } else if (req.url == '/number/even') {
+  }
+  else if (req.url == '/number/even') {
     res.write('{number :[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30]}');
     res.end();
-  } else if (req.url == '/number/rand') {
+  } 
+  else if (req.url == '/number/rand') {
     res.write('{number :[5,17,3,19,76,24,1,5,10,34,8,27,7]}');
     res.end();
-  } else {
+  } 
+  else {
     res.write('try different url');
   }
 });
-
 server.listen(8008);
 console.log('server running at port 8008');
